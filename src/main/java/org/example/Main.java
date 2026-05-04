edb3167244460ba3a9b8576b1992db56646fc137
Renezilla <157187967+Renezilla@users.noreply.github.com>
2026-05-04 20:26:48 -0300

    Adcionado metodo icluir-pedido

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,59 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        static String[] nomes = {
+                "ByteBurger Clássico", "ByteBurger Duplo",
+                "Batata Frita P", "Batata Frita G",
+                "Refrigerante Lata", "Suco Natural", "Água"
+        };
+        static double[] precos = {22.90, 29.90, 12.00, 18.00, 7.00, 10.00, 4.00};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        static double totalGeral = 0;   // caixa geral
+        static double descontoAtual = 0; // desconto do sorteio
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            int opcao;
+
+            do {
+                System.out.println("\n=== MENU PRINCIPAL ===");
+                System.out.println("1 - Novo Pedido");
+                System.out.println("2 - Consultar Cardápio por Categoria");
+                System.out.println("3 - Calcular Troco");
+                System.out.println("4 - Sorteio do Dia");
+                System.out.println("0 - Encerrar");
+                System.out.print("Escolha: ");
+
+                opcao = scanner.nextInt();
+
+                switch (opcao) {
+                    case 1:
+                        novoPedido(scanner);
+                        break;
+                    case 2:
+                        consultarCategoria(scanner);
+                        break;
+                    case 3:
+                        calcularTroco(scanner);
+                        break;
+                    case 4:
+                        sorteio();
+                        break;
+                    case 0:
+                        System.out.println("Encerrando o sistema...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                }
+
+            } while (opcao != 0);
+
+            scanner.close();
         }
+
+
     }
 }
